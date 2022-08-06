ceeca831b6eb18f2a06e5eb0383c6507eb844898
Vladimir <vladsmir323@gmail.com>
2022-08-06 22:50:23 +0300

    data1

---
+++ data_greate.py
@@ -0,0 +1,16 @@
+def name():
+    name = input("Введите Ваше имя")
+    return name
+
+
+def surname():
+    surname = input("Введите Вашу фамилию")
+    return surname
+
+def number_mobile():
+    number_mobile = int(input("Введите Ваш телефон"))
+    return number_mobile
+
+def description():
+    description = input("Введите описание")
+    return description
